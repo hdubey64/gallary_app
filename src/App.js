308453e86f74d23f912012c17e2fd0c400d7a546
Vishal Dubey <hdubey64@gmail.com>
2023-12-05 22:13:38 +0530

    File Name Change

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Hero from "./Components/HeroSection/Hero";
+import { Gallery } from "./Components";
 
 function App() {
    return (
       <div className="App">
          <header className="App-header">
-            <Hero />
+            <Gallery />
          </header>
       </div>
    );
