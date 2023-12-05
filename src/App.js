9858e7c19b9765d182d483fd884d64c3496fe284
Vishal Dubey <hdubey64@gmail.com>
2023-12-05 17:15:20 +0530

    initial setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Hero from "./Components/HeroSection/Hero";
+
+function App() {
+   return (
+      <div className="App">
+         <header className="App-header">
+            <Hero />
+         </header>
+      </div>
+   );
+}
+
+export default App;
