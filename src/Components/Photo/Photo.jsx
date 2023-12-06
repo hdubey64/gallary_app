db14d940e4b8a30b287bcd224c98cd3365e2ac08
Vishal Dubey <hdubey64@gmail.com>
2023-12-06 23:19:46 +0530

    Dynamic Photo

--- src/Components/Photo/Photo.jsx
+++ src/Components/Photo/Photo.jsx
@@ -1,13 +1,12 @@
-import React from "react";
 import { Link } from "react-router-dom";
 
-const Photo = () => {
+const Photo = ({ galleyPic }) => {
    return (
       <div>
          <Link to={`photoDetails/`}>
             <img
-               src="https://img.freepik.com/premium-photo/cute-baby-red-panda-lesser-panda-character-3d-rendering-cartoon-illustration_691560-4376.jpg?size=626&ext=jpg&ga=GA1.1.901052190.1701795903&semt=ais"
-               alt=""
+               src={galleyPic}
+               alt="Gallery Pic"
                className="w-64 rounded-2xl border-4 border-white"
             />
          </Link>
