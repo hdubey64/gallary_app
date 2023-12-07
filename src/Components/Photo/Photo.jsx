6becb09f6118ad1c17a2946b81302271de0dcd01
Vishal Dubey <hdubey64@gmail.com>
2023-12-07 18:15:47 +0530

    Hover Effect

--- src/Components/Photo/Photo.jsx
+++ src/Components/Photo/Photo.jsx
@@ -2,12 +2,18 @@ import { Link } from "react-router-dom";
 
 const Photo = ({ galleyPic, id }) => {
    return (
-      <Link to={`photoDetails/${id}`}>
-         <img
-            src={galleyPic}
-            alt="Gallery Pic"
-            className="w-full md:w-72 rounded md:rounded-lg "
-         />
+      <Link className="relative" to={`photoDetails/${id}`}>
+         <span className="w-full  h-full bg-transparent rounded md:rounded-lg hover:bg-black opacity-40 block  absolute "></span>
+         <div className="z-0">
+            <img
+               src={galleyPic}
+               alt="Gallery Pic"
+               className="w-full md:w-72 rounded border-2 border-blue-500 hover:border-white md:rounded-lg "
+            />
+            <h1 className="absolute top-0 py-2 bg-blue-500 w-6  font-bold left-0">
+               {id}
+            </h1>
+         </div>
       </Link>
    );
 };
